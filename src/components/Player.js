be7e801b57ba5caa4715aaf9f99abce0199bdacd
chinhbean09 <chinh0726@gmail.com>
2023-09-29 13:44:21 +0700

    Components Part 1

---
+++ src/components/Player.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import Players from "../shared/Players"
+
+export default class Player extends Component {
+    render() {
+        return (
+
+            <div className="container player">
+                 <div className="row row-content">
+                 {Players.map((player) => (
+                       
+                            <div className="col col-sm-4 col-md-4" key={player.id}>
+                                <div className="card">
+                                    <img src={player.img} />
+                                    <h3>{player.name}</h3>
+                                    <p className="title">{player.club}</p>
+                                    <p><button className="btn btn-dark btn-lg btn-block">Detail</button></p>
+                                </div>
+                            </div>
+
+                ))}
+
+
+
+                 </div>
+               
+
+
+
+            </div>
+        )
+
+
+
+    }
+}
